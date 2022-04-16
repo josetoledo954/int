2a54da4d742d36028aca6dc6bd1ea1902fa2d26b
josetoledo954 <josetoledo954@gmail.com>
2022-04-16 19:00:34 -0300

    interes

--- scripts/intereses.js
+++ scripts/intereses.js
@@ -12,11 +12,13 @@ const nombreInp = document.getElementById("inpNombre")
 
 
 
-
+// boton de buscar evento
 const boton = document.getElementById("boton")
 
 const botonBuscar = document.getElementById("botonBuscar")
 
+
+// boton de calcular evento
 boton.addEventListener("click", calcular)
 
 botonBuscar.addEventListener("click", buscar)
@@ -35,6 +37,8 @@ botonBuscar.addEventListener("click", buscar)
 //         selector.appendChild(opcion)
 //     })
 // }
+
+// obtener datos de API
 async function getBancos() {
 
     const URLbancos = `https://rickandmortyapi.com/api/character`
@@ -51,7 +55,10 @@ async function getBancos() {
 }
 getBancos()
 
+// selector de personaje
 const inpTasaM = document.getElementById("inpTasaM")
+const img = document.getElementById("imgPer")
+const dialogo = document.getElementById("dialogo")
 
 document.getElementById("inpTasa").addEventListener("click", function (e){
     if (inpTasa.value == "") {
@@ -70,25 +77,12 @@ document.getElementById("inpTasa").addEventListener("click", function (e){
         const indice = data.findIndex((item) => item.id == inpTasa.value)
         console.log("indice", indice);
         if (inpTasa.value != "") {
-            const img = document.getElementById("img")
-            img.innerHTML = ""
-            const opcion = document.createElement("img")
-            opcion.src = (data[indice].image)
-            img.appendChild(opcion)
-            const dialogo = document.createElement("div")
-            dialogo.innerHTML = `
-                <p id= "dialogo"> mi nombre es ${data[indice].name.split(" ")[0] } y voy a calcular tu interes </p>
-                ` 
-            img.appendChild(dialogo)
+            img.src = (data[indice].image)
+            dialogo.innerText = `mi nombre es ${data[indice].name.split(" ")[0] } y voy a calcular tu interes`
         }else{
-            img.innerHTML = `
-                <img src="https://rickandmortyapi.com/api/character/avatar/19.jpeg" alt="">
-                <div>
-                    <p id="dialogo">puedes elegir un personaje</p>
-                </div>
-                `
+            img.src = "https://rickandmortyapi.com/api/character/avatar/19.jpeg"
+            dialogo.innerText = `puedes elegir un personaje`
         }
-        
     }
     getImg()
 })
@@ -101,24 +95,19 @@ const acumuladoLi = document.getElementById("acumulado")
 const interesLi = document.getElementById("interes")
 const nombreLi = document.getElementById("nombre")
 
+// funcion de buscar
 function buscar() {
     imp.innerHTML = ""
     mesLi.innerHTML = ""
     acumuladoLi.innerHTML = ""
     interesLi.innerHTML = ""
     nombreLi.innerHTML = ""
-    img.innerHTML = `
-                <img src="https://rickandmortyapi.com/api/character/avatar/19.jpeg" alt="">
-                <div>
-                    <p id="dialogo">puedes elegir un personaje</p>
-                </div>
-                `
+    img.src = "https://rickandmortyapi.com/api/character/avatar/19.jpeg"
+    dialogo.innerText = `puedes elegir un personaje`
     
     const buscarInp = document.getElementById("inpBuscar")
     const buscar = buscarInp.value
     
-   
-
     const nombreDiv = document.createElement ("div")
     nombreDiv.innerText = `seleccione una de las coincidencias para la busqueda de: ${buscarInp.value}`
     nombreLi.appendChild(nombreDiv)
@@ -143,17 +132,17 @@ function buscar() {
     const nombresLi = document.querySelectorAll("#nombresLista")
     nombresLi.forEach((nombresLista) => nombresLista.addEventListener("click", mostrar))
 
-
+    // funcion de mostrar busqueda
     function mostrar(e) {
         e.preventDefault()
         const nombreMostrar = e.target.innerText
         console.log(nombreMostrar);
 
         imp.innerHTML = ""
-    mesLi.innerHTML = ""
-    acumuladoLi.innerHTML = ""
-    interesLi.innerHTML = ""
-    nombreLi.innerHTML = ""
+        mesLi.innerHTML = ""
+        acumuladoLi.innerHTML = ""
+        interesLi.innerHTML = ""
+        nombreLi.innerHTML = ""
 
         const storage = JSON.parse(localStorage.getItem(nombreMostrar))
 
@@ -172,15 +161,15 @@ function buscar() {
             interesLi.appendChild(interesDiv)
             })
 
-            const nombreDiv = document.createElement ("div")
-    nombreDiv.innerText = `nombre: ${nombreMostrar}`
-    nombreLi.appendChild(nombreDiv)
-    
-
-    console.log((storage));
+        const nombreDiv = document.createElement ("div")
+        nombreDiv.innerText = `nombre: ${nombreMostrar}`
+        nombreLi.appendChild(nombreDiv)
+        
+        console.log((storage));
     }
 }
 
+// funcion de calcular
 function calcular() {
     
 let tasaUso = 0
@@ -205,144 +194,106 @@ let tasaUso = 0
     
     if (montoInp.value != "" && mesesInp.value != "" && tasaUso.value != "" && nombreInp.value != "" && indice <= 0) {
 
-    let monto = parseInt(montoInp.value)
-    let meses = parseInt(mesesInp.value)
-    let tasaN = parseInt(tasaUso.value)
-    let nombre = nombreInp.value
-    
-    let j = 0
-    let intAcum = 0
-    let tasa = tasaN / 100 / 12
-    let interes = 0
-    
-    interesesTotales = []
-    mesesTotales = []
-    montosTotales = []
-
-    imp.innerHTML = ""
-    mesLi.innerHTML = ""
-    acumuladoLi.innerHTML = ""
-    interesLi.innerHTML = ""
-    nombreLi.innerHTML = ""
-
-    for (let i = monto; j < meses ; i = i + (i * tasa)) {
+        let monto = parseInt(montoInp.value)
+        let meses = parseInt(mesesInp.value)
+        let tasaN = parseInt(tasaUso.value)
+        let nombre = nombreInp.value
         
-        j ++
-        interes = i * tasa
-        let acumulado = i + interes
-        intAcum = intAcum + interes
-        interesesTotales.push (intAcum.toFixed(2))
-        mesesTotales.push (j)
-        montosTotales.push (acumulado.toFixed(2))
-        console.log("mes " + j);
-        console.log("acumulado " + acumulado.toFixed(2));
-        console.log("interes " + interes.toFixed(2));
-        console.log("intereses acumulados " + intAcum.toFixed(2));
-        console.log("");
-    }
-    
-    console.log("se calcularon intereses para " + monto + " a " + meses + " meses " + "con una tasa del " + tasaN);
-    console.log("");
-    
-    
-    let interesObtenido = interesesTotales[interesesTotales.length - 1] / monto * 100
-    
-    console.log("el interes obtenido es de " + interesObtenido.toFixed(2));
-    
-    class Intereses {
-        constructor (monto, meses, intereses){
-            this.monto = monto
-            this.meses = meses
-            this.interes = intereses
+        let j = 0
+        let intAcum = 0
+        let tasa = tasaN / 100 / 12
+        let interes = 0
+        
+        interesesTotales = []
+        mesesTotales = []
+        montosTotales = []
+
+        imp.innerHTML = ""
+        mesLi.innerHTML = ""
+        acumuladoLi.innerHTML = ""
+        interesLi.innerHTML = ""
+        nombreLi.innerHTML = ""
+        
+        for (let i = monto; j < meses ; i = i + (i * tasa)) {
+            
+            j ++
+            interes = i * tasa
+            let acumulado = i + interes
+            intAcum = intAcum + interes
+            interesesTotales.push (intAcum.toFixed(2))
+            mesesTotales.push (j)
+            montosTotales.push (acumulado.toFixed(2))
+            console.log("mes " + j);
+            console.log("acumulado " + acumulado.toFixed(2));
+            console.log("interes " + interes.toFixed(2));
+            console.log("intereses acumulados " + intAcum.toFixed(2));
+            console.log("");
         }
-    }
-    
-    let lista = []
-    for (const mes of mesesTotales) {
-        let posicion = mes - 1 
-        let monT = parseFloat(montosTotales[posicion])
-        let intT = parseFloat(interesesTotales[posicion])
-        lista.push(new Intereses(monT,mes, intT))
-    }
-    console.log(lista);
-    
-    const porMes = lista.find (p => p.meses == 2)
-    console.log("busqueda por find " , porMes);
-    
-    const mayor = lista.filter( p => p.interes > 10 )
-    console.log("busqueda por filter " , mayor);
-
-    // const list = document.createElement("li")
-    // list.innerText = ("se calcularon intereses para " + monto + " a " + meses + " meses " + "con una tasa del " + tasaN)
-    
-    // const intOb = document.createElement("li")
-    // intOb.innerText = ("el interes ontenido es de " + interesObtenido.toFixed(2))
-
-    lista.forEach((e) => {
         
-        const mesDiv = document.createElement ("div")
-        mesDiv.innerHTML = e.meses
-        mesLi.appendChild(mesDiv)
+        console.log("se calcularon intereses para " + monto + " a " + meses + " meses " + "con una tasa del " + tasaN);
+        console.log("");
+        
+        
+        let interesObtenido = interesesTotales[interesesTotales.length - 1] / monto * 100
+        
+        console.log("el interes obtenido es de " + interesObtenido.toFixed(2));
+        
+        class Intereses {
+            constructor (monto, meses, intereses){
+                this.monto = monto
+                this.meses = meses
+                this.interes = intereses
+            }
+        }
+        
+        let lista = []
+        for (const mes of mesesTotales) {
+            let posicion = mes - 1 
+            let monT = parseFloat(montosTotales[posicion])
+            let intT = parseFloat(interesesTotales[posicion])
+            lista.push(new Intereses(monT,mes, intT))
+        }
+        console.log(lista);
+        
+        const porMes = lista.find (p => p.meses == 2)
+        console.log("busqueda por find " , porMes);
+        
+        const mayor = lista.filter( p => p.interes > 10 )
+        console.log("busqueda por filter " , mayor);
+
+        lista.forEach((e) => {
             
-        const acumuladoDiv = document.createElement ("div")
-        acumuladoDiv.innerText = e.monto
-        acumuladoLi.appendChild(acumuladoDiv)
-            
-        const interesDiv = document.createElement ("div")
-        interesDiv.innerText = e.interes
-        interesLi.appendChild(interesDiv)
+            const mesDiv = document.createElement ("div")
+            mesDiv.innerHTML = e.meses
+            mesLi.appendChild(mesDiv)
+                
+            const acumuladoDiv = document.createElement ("div")
+            acumuladoDiv.innerText = e.monto
+            acumuladoLi.appendChild(acumuladoDiv)
+                
+            const interesDiv = document.createElement ("div")
+            interesDiv.innerText = e.interes
+            interesLi.appendChild(interesDiv)
         })
 
         const nombreDiv = document.createElement ("div")
         nombreDiv.innerText = `nombre: ${nombre}`
         nombreLi.appendChild(nombreDiv)
 
-    // lista.forEach((e) => {
-    //     const intLi = document.createElement ("div")
-    //     intLi.innerHTML = `
-        
-    //         <div>mes ${e.meses}</div>
-    //         <div>acumulado ${e.monto}</div>
-    //         <div>interes ${e.interes}</div>
-        
-    //     `
-    //     imp.appendChild(intLi)
-    // })
-    dialogo.innerHTML = `
-    <p>para ${monto} a ${meses} meses el con una tasa del ${tasaN}</p>
-    <p>el interes obtenido es de ${interesObtenido.toFixed(2)} </p>
-    ` 
-    // imp.appendChild(intOb)
-    // imp.appendChild(list)
-            
-            montoInp.value = ""
-            mesesInp.value = ""
-            tasaInp.value = ""
-            nombreInp.value = ""
-            inpTasaM.value = ""
-            localStorage.setItem(nombre, JSON.stringify(lista))
-            console.log(nombre.replace(/ /g, ""));
-        }
-    // localStorage.setItem("listaStorage", JSON.stringify(lista))
+        dialogo.innerHTML = `
+        <p>para un monto de ${monto} a ${meses} meses el con una tasa del ${tasaN}</p>
+        <p>el interes obtenido es de ${interesObtenido.toFixed(2)} </p>
+        ` 
+                
+                montoInp.value = ""
+                mesesInp.value = ""
+                tasaInp.value = ""
+                nombreInp.value = ""
+                inpTasaM.value = ""
+                localStorage.setItem(nombre, JSON.stringify(lista))
+                console.log(nombre.replace(/ /g, ""));
+    }
 }
-
-
-// localStorage.setItem("productos", JSON.stringify(productos))
-
-// const productosJson = localStorage.getItem("productos")
-
-// console.log(typeof productosJson);
-
-
-// boton.addEventListener("click", () => {
-//     const input = document.getElementById("entrada")
-//     const nombreProducto = input.value 
-//     const nuevoProducto = document.createElement ("li")
-//     nuevoProducto.innerText = nombreProducto
-//     const root = document.getElementById("root")
-//     root.appendChild(nuevoProducto)
-// })
-
 
 
 
